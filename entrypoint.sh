2207308adb9c59984e350b2593b3764c0819a76c
Gontran Sion <gontran.sion@gmail.com>
2022-03-30 16:27:58 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
+eval "git clone -b warrior git://git.yoctoproject.org/poky"
+eval "cd poky"
+eval "source oe-init-build-env"
+eval "bitbake core-image-minimal"
+
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
